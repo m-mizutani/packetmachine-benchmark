bfd3c45d13707447fc0c7c9df9047dedd9634a2b
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2017-10-22 14:00:04 +0900

    temporary commit

--- src/pm-bench.cc
+++ src/pm-bench.cc
@@ -2,5 +2,15 @@
 #include<packetmachine.hpp>
 
 int main(int argc, char* argv[]) {
+  auto m = pm::Machine();
+  m.add_pcapfile(argv[1]);
+  int ssn_count = 0;
+
+  m.on("TCP.new_session", [&](const pm::Property& p) {
+      ssn_count++;
+    });
+  m.loop();
+
+  std::cout << ssn_count << std::endl;
   return 0;
 }
--- src/tins-bench.cc
+++ src/tins-bench.cc
@@ -1,5 +1,31 @@
 #include<iostream>
+#include <tins/tins.h>
+#include <tins/tcp_ip/stream_follower.h>
+
+
+static size_t ssn_count(0);
+
+bool callback(const Tins::PDU &pdu) {
+  // Find the TCP layer
+  const Tins::TCP &tcp = pdu.rfind_pdu<Tins::TCP>();
+  if (tcp.dport() == 80) {
+    // tcp_count++;
+  }
+  return true;
+}
+
+void on_new_connection(Tins::TCPIP::Stream::Stream& stream) {
+  ssn_count++;
+}
+
 
 int main(int argc, char* argv[]) {
+  Tins::TCPIP::StreamFollower follower;
+  follower.new_stream_callback(&on_new_connection);
+  Tins::FileSniffer(argv[1]).sniff_loop([&](Tins::Packet& packet) {
+      follower.process_packet(packet);
+      return true;
+    });
+  std::cout << ssn_count << std::endl;
   return 0;
 }
