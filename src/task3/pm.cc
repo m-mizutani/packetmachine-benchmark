ba5d38ab39b7551ff699b18dc911a13bc0d0e1f9
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2017-10-25 06:21:46 +0000

    add new task

---
+++ src/task3/pm.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<packetmachine.hpp>
+#include "../ticker.hpp"
+
+int main(int argc, char* argv[]) {
+  int google_count = 0;
+
+  pm::Config config;
+  config.set_false("TCP.enable_session_mgmt");
+
+  auto m = pm::Machine(config);
+  m.add_pcapfile(argv[1]);
+
+  m.on("DNS", [&](const pm::Property& p) {
+      const auto& vals = p.value("DNS.question");
+      if (vals.is_array()) {
+        for (size_t idx = 0; idx < vals.size(); idx++) {
+          const auto& v = vals.get(idx);
+          auto s = v.find("name").repr();
+
+          if (s.find(".google.") != std::string::npos) {
+            google_count++;
+          }
+        }
+      }
+    });
+  
+  {
+    Ticker t;
+    m.loop();
+  }
+
+  return 0;
+}
